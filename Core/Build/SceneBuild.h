b25755ce68217ec3cca37432238a7aefc5e62e87
Hidd3n-47 <122316898+Hidd3n-47@users.noreply.github.com>
2026-04-14 12:21:50 +0100

    126 Game build's with immutable entities Added the scene build and updated the premake file to what is now needed for the build process.

---
+++ Core/Build/SceneBuild.h
@@ -0,0 +1,160 @@
+#pragma once
+
+#ifndef DEV_CONFIGURATION
+
+#include <ScarletCore/Defines.h>
+
+#include <ScarlEnt/Scene.h>
+#include <ScarlEnt/Registry.h>
+#include <ScarlEnt/MutableEntityHandle.h>
+
+#include <ScarletEngine/Core/Filepath.h>
+
+#include <ScarletCoreEcs/Components/Generated/Register.generated.h>
+
+namespace Scarlet
+{
+
+inline void InitGameScene()
+{
+    Filepath::SetEngineDirectory("C:/Users/chris/Downloads/Shadow3D/EngineResources");
+    Filepath::SetProjectDirectory("C:/Users/chris/Downloads/Shadow3D/GameResources");
+
+    Engine::Instance().LoadScarletAssets();
+    
+    WeakHandle<ScarlEnt::Scene> scene = ScarlEnt::Registry::Instance().GetScene("GameScene");
+
+    {
+        ScarlEnt::MutableEntityHandle ent = scene->AddMutableEntity();
+        auto& Transform = ent.AddComponent<Component::Transform>();
+        Transform.translation = Math::Vec3{ 0.000000,35.000000,-1.500000 };
+        Transform.rotation = Math::Vec3{ 0.000000,0.000000,0.000000 };
+        Transform.scale = Math::Vec3{ 1.000000,1.000000,1.000000 };
+        auto& StaticMesh = ent.AddComponent<Component::StaticMesh>();
+        StaticMesh.mesh = AssetRef{ 0,37310013715408072 };
+        StaticMesh.material = AssetRef{ 2,33630958529845530 };
+    }
+
+   {
+        Component::Transform Transform{};
+        Transform.translation = Math::Vec3{ -14.500000,0.500000,2.000000 };
+        Transform.rotation = Math::Vec3{ 0.000000,0.000000,0.000000 };
+        Transform.scale = Math::Vec3{ 0.200000,1.000000,1.000000 };
+        Component::DroneSpawner DroneSpawner{};
+        Component::StaticMesh StaticMesh{};
+        StaticMesh.mesh = AssetRef{ 0,0 };
+        StaticMesh.material = AssetRef{ 2,0 };
+
+        scene->AddEntity<Component::Transform, Component::DroneSpawner, Component::StaticMesh>(std::move(Transform), std::move(DroneSpawner), std::move(StaticMesh));
+    }
+
+   {
+        Component::Transform Transform{};
+        Transform.translation = Math::Vec3{ 0.000000,0.000000,-0.850000 };
+        Transform.rotation = Math::Vec3{ 0.000000,0.000000,0.000000 };
+        Transform.scale = Math::Vec3{ 1.000000,1.000000,1.000000 };
+        Component::StaticMesh StaticMesh{};
+        StaticMesh.mesh = AssetRef{ 0,37314080891006766 };
+        StaticMesh.material = AssetRef{ 2,37314269018342713 };
+
+        scene->AddEntity<Component::Transform, Component::StaticMesh>(std::move(Transform), std::move(StaticMesh));
+    }
+
+   {
+        Component::Transform Transform{};
+        Transform.translation = Math::Vec3{ 0.000000,-3.000000,-33.000000 };
+        Transform.rotation = Math::Vec3{ 90.000000,0.000000,0.000000 };
+        Transform.scale = Math::Vec3{ 1.000000,1.000000,1.000000 };
+        Component::StaticMesh StaticMesh{};
+        StaticMesh.mesh = AssetRef{ 0,37310013715408072 };
+        StaticMesh.material = AssetRef{ 2,37322288106903960 };
+
+        scene->AddEntity<Component::Transform, Component::StaticMesh>(std::move(Transform), std::move(StaticMesh));
+    }
+
+   {
+        Component::Transform Transform{};
+        Transform.translation = Math::Vec3{ 0.000000,25.000000,-33.000000 };
+        Transform.rotation = Math::Vec3{ 90.000000,0.000000,0.000000 };
+        Transform.scale = Math::Vec3{ 1.000000,1.000000,1.000000 };
+        Component::StaticMesh StaticMesh{};
+        StaticMesh.mesh = AssetRef{ 0,37310013715408072 };
+        StaticMesh.material = AssetRef{ 2,37322288106903960 };
+
+        scene->AddEntity<Component::Transform, Component::StaticMesh>(std::move(Transform), std::move(StaticMesh));
+    }
+
+   {
+        Component::Transform Transform{};
+        Transform.translation = Math::Vec3{ 15.000000,0.000000,-33.000000 };
+        Transform.rotation = Math::Vec3{ 90.000000,0.000000,90.000000 };
+        Transform.scale = Math::Vec3{ 1.000000,1.000000,1.000000 };
+        Component::StaticMesh StaticMesh{};
+        StaticMesh.mesh = AssetRef{ 0,37310013715408072 };
+        StaticMesh.material = AssetRef{ 2,37322288106903960 };
+
+        scene->AddEntity<Component::Transform, Component::StaticMesh>(std::move(Transform), std::move(StaticMesh));
+    }
+
+   {
+        Component::Transform Transform{};
+        Transform.translation = Math::Vec3{ -15.000000,0.000000,-33.000000 };
+        Transform.rotation = Math::Vec3{ 90.000000,0.000000,90.000000 };
+        Transform.scale = Math::Vec3{ 1.000000,1.000000,1.000000 };
+        Component::StaticMesh StaticMesh{};
+        StaticMesh.mesh = AssetRef{ 0,37310013715408072 };
+        StaticMesh.material = AssetRef{ 2,37322288106903960 };
+
+        scene->AddEntity<Component::Transform, Component::StaticMesh>(std::move(Transform), std::move(StaticMesh));
+    }
+
+    {
+        ScarlEnt::MutableEntityHandle ent = scene->AddMutableEntity();
+        auto& Transform = ent.AddComponent<Component::Transform>();
+        Transform.translation = Math::Vec3{ 12.000000,-2.500000,2.000000 };
+        Transform.rotation = Math::Vec3{ 0.000000,0.000000,0.000000 };
+        Transform.scale = Math::Vec3{ 1.000000,0.200000,1.000000 };
+        auto& DroneSpawner = ent.AddComponent<Component::DroneSpawner>();
+        auto& StaticMesh = ent.AddComponent<Component::StaticMesh>();
+        StaticMesh.mesh = AssetRef{ 0,0 };
+        StaticMesh.material = AssetRef{ 2,0 };
+    }
+
+    {
+        ScarlEnt::MutableEntityHandle ent = scene->AddMutableEntity();
+        auto& Transform = ent.AddComponent<Component::Transform>();
+        Transform.translation = Math::Vec3{ 0.000000,24.500000,2.000000 };
+        Transform.rotation = Math::Vec3{ 0.000000,0.000000,0.000000 };
+        Transform.scale = Math::Vec3{ 1.000000,0.200000,1.000000 };
+        auto& DroneSpawner = ent.AddComponent<Component::DroneSpawner>();
+        auto& StaticMesh = ent.AddComponent<Component::StaticMesh>();
+        StaticMesh.mesh = AssetRef{ 0,0 };
+        StaticMesh.material = AssetRef{ 2,0 };
+    }
+
+   {
+        Component::Transform Transform{};
+        Transform.translation = Math::Vec3{ 0.000000,0.000000,0.000000 };
+        Transform.rotation = Math::Vec3{ 0.000000,0.000000,0.000000 };
+        Transform.scale = Math::Vec3{ 1.000000,1.000000,1.000000 };
+        Component::WaveManager WaveManager{};
+        WaveManager.wave = uint32{ 0 };
+        WaveManager.numberOfEnemiesToSpawn = uint32{ 5 };
+        WaveManager.numberOfSpawnedEnemies = uint32{ 0 };
+        WaveManager.maxNumberOfEnemies = uint32{ 30 };
+        WaveManager.maxEnemySpeed = float{ 0.100000 };
+        WaveManager.startingEnemySpeed = float{ 0.020000 };
+        WaveManager.timeBetweenWaves = float{ 2.000000 };
+        WaveManager.timeBetweenSpawns = float{ 2.000000 };
+        WaveManager.startingEnemyHealth = float{ 40.000000 };
+        WaveManager.currentTimer = float{ 2.000000 };
+
+        scene->AddEntity<Component::Transform, Component::WaveManager>(std::move(Transform), std::move(WaveManager));
+    }
+
+}
+
+} // Namespace Scarlet.
+
+#endif // DEV_CONFIGURATION.
+
--- premake5.lua
+++ premake5.lua
@@ -14,8 +14,8 @@ project "Shadow3D"
     staticruntime "Off"
     cppdialect "C++20"
 
-    targetdir(path.join("E:/Programming/ScarletProjects/Shadow3D/", "Scratch/Bin/", "%{prj.name}"))
-    objdir(path.join("E:/Programming/ScarletProjects/Shadow3D/", "Scratch/Bin-Int/", "%{prj.name}"))
+    targetdir(path.join("E:/Programming/ScarletProjects/Shadow3D/", "Scratch", "Bin", "%{cfg.buildcfg}", "%{prj.name}"))
+    objdir(path.join("E:/Programming/ScarletProjects/Shadow3D/", "Scratch", "Bin-Int", "%{cfg.buildcfg}", "%{prj.name}"))
 
     files
     {
@@ -38,18 +38,6 @@ project "Shadow3D"
         "$(SolutionDir)",
     }
 
-    links
-    {
-        "E:/Programming/ScarletEngine/Bin/ScarlEnt.lib",
-        "E:/Programming/ScarletEngine/Bin/ScarlEnt.dll",
-        "E:/Programming/ScarletEngine/Bin/ScarletMath.lib",
-        "E:/Programming/ScarletEngine/Bin/ScarletCore.lib",
-        "E:/Programming/ScarletEngine/Bin/ScarletReflect.lib",
-        "E:/Programming/ScarletEngine/Bin/ScarletCoreEcs.lib",
-        "E:/Programming/ScarletEngine/Bin/ScarletEngine.lib",
-        "E:/Programming/ScarletEngine/Bin/ScarletEngine.dll"
-    }
-
     filter "system:windows"
         systemversion "latest"
 
@@ -57,8 +45,30 @@ project "Shadow3D"
         runtime "Debug"
         defines { "DEV_CONFIGURATION", "GAME_DLL_BUILD" }
         symbols "On"
+        links
+        {
+            "E:/Programming/ScarletEngine/Bin/ScarlEnt.lib",
+            "E:/Programming/ScarletEngine/Bin/ScarlEnt.dll",
+            "E:/Programming/ScarletEngine/Bin/ScarletMath.lib",
+            "E:/Programming/ScarletEngine/Bin/ScarletCore.lib",
+            "E:/Programming/ScarletEngine/Bin/ScarletReflect.lib",
+            "E:/Programming/ScarletEngine/Bin/ScarletCoreEcs.lib",
+            "E:/Programming/ScarletEngine/Bin/ScarletEngine.lib",
+            "E:/Programming/ScarletEngine/Bin/ScarletEngine.dll"
+        }
 
     filter "configurations:Release"
         runtime "Release"
         defines "GAME_DLL_BUILD"
         optimize "On"
+        links
+        {
+            "E:/Programming/ScarletEngine/Build/Bin/ScarlEnt.lib",
+            "E:/Programming/ScarletEngine/Build/Bin/ScarlEnt.dll",
+            "E:/Programming/ScarletEngine/Build/Bin/ScarletMath.lib",
+            "E:/Programming/ScarletEngine/Build/Bin/ScarletCore.lib",
+            "E:/Programming/ScarletEngine/Build/Bin/ScarletReflect.lib",
+            "E:/Programming/ScarletEngine/Build/Bin/ScarletCoreEcs.lib",
+            "E:/Programming/ScarletEngine/Build/Bin/ScarletEngine.lib",
+            "E:/Programming/ScarletEngine/Build/Bin/ScarletEngine.dll"
+        }
